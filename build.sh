fad666b488be48cf000cac8713442d09ddc0b51f
wunnr <wunner@protonmail.com>
2025-05-09 23:29:12 -0600

    moved the kwin launcher script

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
 cp target/release/partydeck-rs PartyDeckKWinLaunch.sh build/ && \
+cp res/PartyDeckKWinLaunch.sh build/ && \
 cp res/splitscreen_kwin.js res/wine_disable_hidraw.reg build/res
