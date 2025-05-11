2bb4d331b084054b998f7b6b87523b4c63ae3bcf
wunnr <wunner@protonmail.com>
2025-05-10 18:25:19 -0600

    kwin launcher also opens up konsole for debugging

--- res/PartyDeckKWinLaunch.sh
+++ res/PartyDeckKWinLaunch.sh
@@ -4,4 +4,4 @@ resolution=$(xrandr | grep '*' | awk '{print $1}')
 width=$(echo $resolution | cut -d 'x' -f 1)
 height=$(echo $resolution | cut -d 'x' -f 2)
 
-kwin_wayland --xwayland --width $width --height $height --exit-with-session "./partydeck-rs"
+kwin_wayland --xwayland --width $width --height $height --exit-with-session "konsole -e ./partydeck-rs"
