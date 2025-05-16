d56bc243e47dfbc373e6a9b179be5c6eabfed371
wunnr <wunner@protonmail.com>
2025-05-15 21:41:05 -0600

    Improve error handling, allow fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod app;
+mod game;
 mod handler;
 mod input;
 mod launch;
@@ -8,7 +9,6 @@ mod util;
 use crate::app::*;
 use crate::paths::*;
 use crate::util::*;
-use dialog::DialogBox;
 
 fn main() -> eframe::Result {
     std::fs::create_dir_all(PATH_PARTY.join("gamesyms"))
@@ -24,30 +24,44 @@ fn main() -> eframe::Result {
         std::fs::remove_dir_all(PATH_PARTY.join("tmp")).unwrap();
     }
     if !PATH_RES.join("umu-run").exists() {
-        let _ = dialog::Message::new(
+        msg(
+            "Downloading Dependencies",
             "UMU Launcher not found in resources folder. PartyDeck uses UMU to launch Windows games with Proton. Click OK to automatically download from the internet.",
-        )
-        .title("Download UMU")
-        .show()
-        .unwrap();
-        update_umu_launcher().unwrap();
+        );
+        if let Err(e) = update_umu_launcher() {
+            println!("Failed to download UMU Launcher: {}", e);
+            msg("Error", &format!("Failed to download UMU Launcher: {}", e));
+            return Ok(());
+        }
     }
     if !PATH_RES.join("goldberg_linux").exists() || !PATH_RES.join("goldberg_win").exists() {
-        let _ = dialog::Message::new(
+        msg(
+            "Downloading Dependencies",
             "Goldberg Steam Emu not found in resources folder. PartyDeck uses Goldberg for LAN play. Click OK to automatically download from the internet.",
-        )
-        .title("Download Goldberg")
-        .show()
-        .unwrap();
-        update_goldberg_emu().unwrap();
+        );
+        if let Err(e) = update_goldberg_emu() {
+            println!("Failed to download Goldberg: {}", e);
+            msg("Error", &format!("Failed to download Goldberg: {}", e));
+            return Ok(());
+        }
     }
 
     println!("\n[PARTYDECK] started\n");
+
+    let fullscreen = std::env::args().any(|arg| arg == "--fullscreen");
+
+    let (_, scrheight) = get_screen_resolution();
+
+    let scale = match fullscreen {
+        true => scrheight as f32 / 560.0,
+        false => 1.3,
+    };
 
     let options = eframe::NativeOptions {
         viewport: eframe::egui::ViewportBuilder::default()
             .with_inner_size([1080.0, 540.0])
             .with_min_inner_size([640.0, 360.0])
+            .with_fullscreen(fullscreen)
             .with_icon(
                 eframe::icon_data::from_png_bytes(&include_bytes!("../res/icon.png")[..])
                     .expect("Failed to load icon"),
@@ -60,7 +74,7 @@ fn main() -> eframe::Result {
         Box::new(|cc| {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
-            cc.egui_ctx.set_zoom_factor(1.3);
+            cc.egui_ctx.set_zoom_factor(scale);
             Ok(Box::<PartyApp>::default())
         }),
     )
