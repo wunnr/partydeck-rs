948c4316d7a7d277e533af647984ef1e286b72ff
wunnr <wunner@protonmail.com>
2025-06-02 14:54:08 -0600

    remove hidraw reg patch

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
 cp target/release/partydeck-rs res/PartyDeckKWinLaunch.sh build/ && \
-cp res/splitscreen_kwin.js res/wine_disable_hidraw.reg build/res
+cp res/splitscreen_kwin.js build/res
