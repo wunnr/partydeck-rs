d75f7959d9e76f6bdae0dcd42124497bcbd49fb1
wunnr <wunner@protonmail.com>
2025-07-13 16:49:21 -0600

    gamescope keepAbove on active

--- res/splitscreen_kwin_vertical.js
+++ res/splitscreen_kwin_vertical.js
@@ -22,54 +22,72 @@ Ypos_4p = [0, 0, scrheight / 2, scrheight / 2];
 Xsize_4p = [scrwidth / 2, scrwidth / 2, scrwidth / 2, scrwidth / 2];
 Ysize_4p = [scrheight / 2, scrheight / 2, scrheight / 2, scrheight / 2];
 
-function gamescopeSplitscreen(){
-    var allClients = workspace.windowList();
-    var gamescopeClients = []
+function getGamescopeClients() {
+  var allClients = workspace.windowList();
+  var gamescopeClients = [];
 
-    for (var i = 0; i < allClients.length; i++){
-        if (allClients[i].resourceClass == 'gamescope'){
-            gamescopeClients.push(allClients[i]);
-        }
+  for (var i = 0; i < allClients.length; i++) {
+    if (allClients[i].resourceClass == "gamescope") {
+      gamescopeClients.push(allClients[i]);
     }
-    switch (gamescopeClients.length){
-        case 0:
-            return;
-        case 1:
-            var Xpos = Xpos_1p;
-            var Ypos = Ypos_1p;
-            var Xsize = Xsize_1p;
-            var Ysize = Ysize_1p;
-            break;
-        case 2:
-            var Xpos = Xpos_2p;
-            var Ypos = Ypos_2p;
-            var Xsize = Xsize_2p;
-            var Ysize = Ysize_2p;
-            break;
-        case 3:
-            var Xpos = Xpos_3p;
-            var Ypos = Ypos_3p;
-            var Xsize = Xsize_3p;
-            var Ysize = Ysize_3p;
-            break;
-        case 4:
-            var Xpos = Xpos_4p;
-            var Ypos = Ypos_4p;
-            var Xsize = Xsize_4p;
-            var Ysize = Ysize_4p;
-            break;
-    }
+  }
+  return gamescopeClients;
+}
 
-    for (var i = 0; i < gamescopeClients.length; i++){
-        gamescopeClients[i].noBorder = true;
-        gamescopeClients[i].frameGeometry = {
-            x: Xpos[i],
-            y: Ypos[i],
-            width: Xsize[i],
-            height: Ysize[i]
-        };
+function gamescopeAboveBelow() {
+  var gamescopeClients = getGamescopeClients();
+  for (var i = 0; i < gamescopeClients.length; i++) {
+    if (workspace.activeWindow.resourceClass == "gamescope") {
+      gamescopeClients[i].keepAbove = true;
+    } else {
+      gamescopeClients[i].keepAbove = false;
     }
+  }
+}
+
+function gamescopeSplitscreen() {
+  var gamescopeClients = getGamescopeClients();
+
+  switch (gamescopeClients.length) {
+    case 0:
+      return;
+    case 1:
+      var Xpos = Xpos_1p;
+      var Ypos = Ypos_1p;
+      var Xsize = Xsize_1p;
+      var Ysize = Ysize_1p;
+      break;
+    case 2:
+      var Xpos = Xpos_2p;
+      var Ypos = Ypos_2p;
+      var Xsize = Xsize_2p;
+      var Ysize = Ysize_2p;
+      break;
+    case 3:
+      var Xpos = Xpos_3p;
+      var Ypos = Ypos_3p;
+      var Xsize = Xsize_3p;
+      var Ysize = Ysize_3p;
+      break;
+    case 4:
+      var Xpos = Xpos_4p;
+      var Ypos = Ypos_4p;
+      var Xsize = Xsize_4p;
+      var Ysize = Ysize_4p;
+      break;
+  }
+
+  for (var i = 0; i < gamescopeClients.length; i++) {
+    gamescopeClients[i].noBorder = true;
+    gamescopeClients[i].frameGeometry = {
+      x: Xpos[i],
+      y: Ypos[i],
+      width: Xsize[i],
+      height: Ysize[i],
+    };
+  }
 }
 
 workspace.windowAdded.connect(gamescopeSplitscreen);
 workspace.windowRemoved.connect(gamescopeSplitscreen);
+workspace.windowActivated.connect(gamescopeAboveBelow);
