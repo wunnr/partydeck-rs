50e97a4f7e3215979007d23a0a5efaed8ee198d8
wunnr <wunner@protonmail.com>
2025-07-12 22:30:57 -0600

    build grabs gamescope build from submodule folder

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
 cp target/release/partydeck-rs res/PartyDeckKWinLaunch.sh build/ && \
-cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js res/gamescope build/res
+cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js build/res && \
+cp deps/gamescope/build/src/gamescope build/res
