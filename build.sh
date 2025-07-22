82d63f8b91b02d9a0acf9e04a0d360e986913081
wunnr <wunner@protonmail.com>
2025-07-21 20:49:35 -0600

    No more kwin shell script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
-cp target/release/partydeck-rs res/PartyDeckKWinLaunch.sh build/ && \
+cp target/release/partydeck-rs build/ && \
 cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js build/res && \
 cp deps/gamescope/build/src/gamescope build/res
