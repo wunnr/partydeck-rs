c09ce03726851805e80ad9bf3bcec99e16461079
wunnr <wunner@protonmail.com>
2025-07-22 20:46:11 -0600

    remove -rs from program name

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cargo build --release && \
-rm -rf build/partydeck-rs
+rm -rf build/partydeck
 mkdir -p build/ build/res build/bin && \
-cp target/release/partydeck-rs build/ && \
+cp target/release/partydeck build/ && \
 cp LICENSE build/ && cp COPYING.md build/thirdparty.txt && \
 cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js build/res && \
 cp deps/gamescope/build/src/gamescope build/bin/gamescope-kbm
