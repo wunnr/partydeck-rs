21d3d87abd3b30b785f12afca880def3ef00948f
wunnr <wunner@protonmail.com>
2025-07-22 19:46:29 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 cargo build --release && \
 rm -rf build/partydeck-rs
-mkdir -p build/ build/res && \
+mkdir -p build/ build/res build/bin && \
 cp target/release/partydeck-rs build/ && \
+cp LICENSE build/ && cp COPYING.md build/thirdparty.txt && \
 cp res/splitscreen_kwin.js res/splitscreen_kwin_vertical.js build/res && \
-cp deps/gamescope/build/src/gamescope build/res
+cp deps/gamescope/build/src/gamescope build/bin/gamescope-kbm
